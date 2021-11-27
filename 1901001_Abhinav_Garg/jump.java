102307e3548190c09f1eeb710a56fd0f8e5ee4e7
AABHINAAV <66939062+AABHINAAV@users.noreply.github.com>
2021-11-27 17:32:36 +0530

    Add files via upload

---
+++ 1901001_Abhinav_Garg/jump.java
@@ -0,0 +1,10 @@
+package com.example.explicitintent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class jump extends AppCompatActivity {
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+ super.onCreate(savedInstanceState);
+ setContentView(R.layout.activity_jump);
+ }
+}
