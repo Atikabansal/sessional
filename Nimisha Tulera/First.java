ffe10e8701e14448d79e372ccf6828a5416752f7
nimisha345 <85776142+nimisha345@users.noreply.github.com>
2021-11-27 19:25:28 +0530

    Add files via upload

---
+++ Nimisha Tulera/First.java
@@ -0,0 +1,23 @@
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class First extends AppCompatActivity {
+    protected void onCreate(Bundle b){
+        super.onCreate(b);
+        setContentView(R.layout.first_xml);
+
+        Button b1 = findViewById(R.id.btn);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Second.class);
+                startActivity(i);
+            }
+        });
+    }
+}
